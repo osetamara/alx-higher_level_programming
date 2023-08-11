ce96bbe770220e4b2ced01f72bfc241e3731d21f
osetamara <osemudiamebrifini@gmail.com>
2023-08-11 22:02:14 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
+# This script prints the number of and the list of its command-line arguments
 
 if __name__ == "__main__":
     # This block of code is executed when the script is run directly, not when imported.
-
-    # Import the sys module, which provides access to command-line arguments.
+    
     import sys
 
-    # Calculate the number of command-line arguments.
-    count = len(sys.argv) - 1
+    # Retrieve the list of command-line arguments.
+    arg = sys.argv
+    # Calculate the number of arguments provided.
+    size = len(arg) - 1
 
-    # Check how many arguments were passed and print appropriate message.
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+    # Check the number of arguments and print appropriate messages.
+    if size > 1:
+        # If more than one argument is present.
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            # Print each argument's position and value.
+            print("{}: {}".format(i, arg[i]))
+
+    elif size == 0:
+        # If no arguments are provided.
+        print("{} arguments.".format(size))
+
     else:
-        print("{} arguments:".format(count))
-
-    # Iterate through the arguments and print their position and value.
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        # If only one argument is provided.
+        print("{} argument:".format(size))
+        # Print the position and value of the single argument.
+        print("{}: {}".format(size, arg[1]))
