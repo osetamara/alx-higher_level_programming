8ee494df33c5802be1542f8f52ad6d2d2ef13fe2
osetamara <osemudiamebrifini@gmail.com>
2023-08-11 21:40:39 +0000

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-if __number__ == "__main__":
-"""ptint the sum,mutiply, difference and qoutation of 10 and 5 """
-  from calculator_1 import add, sub, mul, div
 
+if __name__ == "__main__":
+    # This block of code is executed when the script is run directly, not when imported.
+
+    # Import necessary functions from the calculator_1 module.
+    from calculator_1 import add, sub, mul, div
+
+    # Define the values of the variables a and b.
     a = 10
     b = 5
 
+    # Print the sum of a and b.
     print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Print the difference of a and b.
     print("{} - {} = {}".format(a, b, sub(a, b)))
+
+    # Print the product of a and b.
     print("{} * {} = {}".format(a, b, mul(a, b)))
+
+    # Print the quotient of a and b.
     print("{} / {} = {}".format(a, b, div(a, b)))
