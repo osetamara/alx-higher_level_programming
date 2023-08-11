05a3f28c669df046888d81c6697ced9c495c9165
osetamara <osemudiamebrifini@gmail.com>
2023-08-11 22:30:41 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-# This block of code is executed when the script is run directly, not when imported.
-    
-# Import the 'hidden_4' module to access its names.
     import hidden_4
- # Get a list of all names defined in the 'hidden_4' module.
-names = dir(hidden_4)
-# Iterate through the names and print those that are not private (don't start with '__').
-    for name in names:
-        if name[:2] != "__":
-            # Print the non-private name.
-            print(name)
+    # Print sorted name from directory
+    for name in sorted(dir(hidden_4)):
+        # print only names that do not start with __
+        if name[:2] != '__':
+            print("{}".format(name))
