5b753395c35b80193596f0b1adbe7c56189b5527
osetamara <osemudiamebrifini@gmail.com>
2023-08-11 23:34:16 +0000

    0-day.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
-    # This block of code is executed when the script is run directly, not when imported.
-    
-    # Import the 'add' function from the 'add_0' module.
+    """Print the sum of 1 and 2."""
     from add_0 import add
 
-    # Define the values of variables 'a' and 'b' for the addition operation.
     a = 1
     b = 2
-
-
-    # Print the formatted output showing the addition operation and result.
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
