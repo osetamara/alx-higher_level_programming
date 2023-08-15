59c89472015a62406301a5985aca0accaad587f5
osetamara <osemudiamebrifini@gmail.com>
2023-08-15 13:41:22 +0000

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,14 +3,13 @@
 def delete_at(my_list=[], idx=0):
     """
     Delete an item from a list at a specific position.
-    Args:
-        my_list (list): list from which to delete an item.
+    Args: my_list (list): list from which to delete an item.
       Defaults to an empty list if not provided.
         idx (int): The index of the item to be deleted.  
     """
-    # Check if the index is within valid bounds
+    #Check if the index is within valid bounds
     if idx >= 0 and idx < len(my_list):
-        # Use the 'del' statement to remove the item at the specified index
+        #Use 'del' statement to remove item at specified index
         del my_list[idx]
     # Return the modified list
     return my_list
