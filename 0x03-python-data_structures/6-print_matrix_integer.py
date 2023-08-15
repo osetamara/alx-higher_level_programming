cd5bc7ea988db0c58b14ea1c20b78ef50395fce7
osetamara <osemudiamebrifini@gmail.com>
2023-08-15 14:53:02 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         #Loop through each element in the current row
         for col in row:
-            #Print each element with proper formatting,use a space separate elements  row
+            #Print each element proper formatting,use space separate elements
             print("{:d}".format(col), end=" " if col != row[-1] else "")
         #Print a new line to move to the next row
         print()
