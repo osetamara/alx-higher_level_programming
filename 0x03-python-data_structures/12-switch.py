e5e076030abb065722037012da53cb65a25bf957
osetamara <osemudiamebrifini@gmail.com>
2023-08-15 11:18:48 +0000

    12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-# Initialize two variables
+#!/usr/bin/python3
 a = 89
 b = 10
-
-# Swap the values of the variables using tuple unpacking
 a, b = b, a
-
-# Print the swapped values
-print("After swapping: a={:d} - b={:d}".format(a, b))
+print("a={:d} - b={:d}".format(a, b))
