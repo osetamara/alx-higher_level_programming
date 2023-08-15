c0a044f462579eba05efa80e44e838ee335a141c
osetamara <osemudiamebrifini@gmail.com>
2023-08-15 13:32:18 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,8 +5,7 @@ def element_at(my_list, idx):
     Retrieve an element from a list at a specified index.    
     Args: my_list (list):list from which to retrieve element.
         idx (int): The index of element to re
-    Return:
-        element: element at a specified index.
+    Return:element: element at a specified index.
     """
     # Check if the index is out of bounds
     if idx < 0 or idx > (len(my_list) - 1):
