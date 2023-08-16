3818441de8297a4d820fe0868b5a0ade647023d2
osetamara <osemudiamebrifini@gmail.com>
2023-08-16 12:17:33 +0000

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    num = len(a_dictionary)  # Get number of keys directly using  len() function
+    return num  # Return the total number of keys
