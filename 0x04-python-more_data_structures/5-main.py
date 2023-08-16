06de5fb3e0ae1c8d59a49ed1e08e09983ba7cd83
osetamara <osemudiamebrifini@gmail.com>
2023-08-16 18:53:36 +0000

    main.py

---
+++ 0x04-python-more_data_structures/5-main.py
@@ -0,0 +1,5 @@
+mport__('5-number_keys').number_keys
+
+a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+nb_keys = number_keys(a_dictionary)
+print("Number of keys: {:d}".format(nb_keys))
