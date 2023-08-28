15b1d5d01bfd0881a98823dc20c6f11fdda0ed02
osetamara <osemudiamebrifini@gmail.com>
2023-08-28 17:31:30 +0000

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """Executes a function safely.
+
+    Args:
+        fct: The function to execute.
+        args: Arguments for fct.
+
+    Returns:
+        result of function is call
+        Otherwise - None
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
+
