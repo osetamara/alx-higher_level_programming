be412a31f9cc4394b7d2d690e819f34b6eecd2d8
osetamara <osemudiamebrifini@gmail.com>
2023-10-19 10:11:20 +0000

    13-change_class.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table from the database hbtn_0c_0
+-- list all the records in descending order ordered by score
+SELECT score, name FROM second_table
+ORDER BY score DESC;
