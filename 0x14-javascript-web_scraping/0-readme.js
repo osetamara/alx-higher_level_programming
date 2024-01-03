18954e3e3a5f7661ce1787f4c6bef3694bf8f838
osetamara <osemudiamebrifini@gmail.com>
2024-01-03 13:23:30 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,15 @@
-t process = require('process');
+#!/usr/bin/node
+const process = require('process');
 const fs = require('fs');
 
-// The first argument is file path
+// The first argument is the file path
 const file = process.argv[2];
-
-// Read the content of specified file in utf-8 encoding
+// The content of the file must be written in utf-8
 fs.readFile(file, 'utf8', function (err, data) {
-  // Check if an error occurred during reading process
+  // If an error occurred during the reading, print the error object
   if (err) {
-    // If an error occurred, print error object to console
     console.log(err);
   } else {
-    // Output content of file to standard output (console)
     process.stdout.write(data);
   }
 });
